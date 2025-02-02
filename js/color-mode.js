b6e222affa1b67ccd2b0d4c2f9a228cc851a1c7a
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-02-02 22:59:47 +0530

    portfolio day2

---
+++ js/color-mode.js
@@ -0,0 +1,7 @@
+// DEFINE DOM ELEMENTS
+
+const toggleButton = document.querySelector("#toggle-button");
+const root = document.querySelector(":root");
+const storageKey = "color-mode"; // to remember user preference
+const defaultMode = "light-mode";
+
