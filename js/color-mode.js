480afef60c93b62ce001bf6b715b8745716c2524
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-02-03 17:59:17 +0530

    portfolio day3

--- js/color-mode.js
+++ js/color-mode.js
@@ -5,3 +5,37 @@ const root = document.querySelector(":root");
 const storageKey = "color-mode"; // to remember user preference
 const defaultMode = "light-mode";
 
+// Load the user's preffered color mode from local storage
+
+function loadColorMode() {
+  const colorMode = localStorage.getItem(storageKey);
+  root.classList.add(colorMode || defaultMode);
+  updateToggleButton();
+  
+}
+
+loadColorMode();
+
+// Toggle the color mode
+toggleButton.addEventListener("click", () => {
+  saveColorMode();
+});
+
+// Save the user's color mode preference to local storage
+function saveColorMode() {
+  const currentMode = root.classList.contains("dark-mode")
+    ? "light-mode"
+    : "dark-mode";
+  root.classList.remove("light-mode", "dark-mode");
+  root.classList.add(currentMode);
+  localStorage.setItem(storageKey, currentMode);
+  updateToggleButton();
+}
+
+function updateToggleButton() {
+  if (root.classList.contains("dark-mode")) {
+    toggleButton.style.backgroundImage = "var(--moon)";
+  } else {
+    toggleButton.style.backgroundImage = "var(--sun)";
+  }
+}
